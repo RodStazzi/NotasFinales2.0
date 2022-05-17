48e0474b3ae284e788a58abadc66dc5f893d049f
rosta <rodolfostazzi@gmail.com>
2022-05-17 15:39:56 -0400

    subiendo 'funcion nota aprobacion', despliegue de tabla y ejecución de frase de aprobación/reprobación

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,5 +97,93 @@ do {
   }
 } while (nota2ramo3 == "" || isNaN(nota2ramo3) || nota2ramo3>7 || nota2ramo3<=0)
 
-
 //FIN DE INGRESO DE DATOS PARA LA TABLA
+
+
+nota1ramo3 = parseFloat(nota1ramo3);
+nota2ramo3 = parseFloat(nota2ramo3);
+//la nota de aprobación pedida arriba, se guarda en notaaprobación y ahora se parsea a float
+notaaprobacion = parseFloat(notaaprobacion);
+//se genera la nota necesaria para aprobar y se guarda en nota3ramo3necesaria
+nota3ramo3necesaria = ((notaaprobacion*3)-(nota1ramo3+nota2ramo3)).toFixed(2);
+
+// DESPLIEGUE DE TABLA CON SUS DATOS *encabezado de la tabla
+document.write("<div class='container mt-3'>");
+document.write("<h1>NOTAS FINALES</h1>");
+// DESPLIEGUE DE TABLA CON SUS DATOS PERSONALES
+document.write("<div>");
+document.write("<table class='table'>");
+document.write("<tbody>");
+document.write("<tr>");
+document.write("<th scope='row'>Nombre</th>");
+document.write("<td>"+ nombre +" "+ apellido +"</td>");
+document.write("<tr>");
+document.write("<tr>");
+document.write("<th scope='row'>Carrera</th>");
+document.write("<td>"+carrera+"</td>");
+document.write("<tr>");
+document.write("</tbody>");
+document.write("</table");
+document.write("</div>");
+
+// DESPLIEGUE DE TABLA CON NOTAS
+document.write("<table class='table'>");
+document.write("<thead class='bg-dark text-white'>");//inicio encabezado a nuestra tabla
+document.write("<tr>");// incio estructura de columnas en nuestra tabla
+document.write("<th scope='col'>Ramo</th>");//definir el nombre o datos que tendrán los encabezados
+document.write("<th scope='col'>Nota 1</th>");
+document.write("<th scope='col'>Nota 2</th>");
+document.write("<th scope='col'>Nota 3</th>");
+document.write("<th scope='col'>Promedio</th>");
+document.write("</tr");
+document.write("</thead>");
+// contenido de la tabla
+document.write("<tbody>");
+document.write("<tr>");
+document.write("<th scope='row'>"+ramo1+"</th>");
+document.write("<td>"+nota1ramo1+"</td>");
+document.write("<td>"+nota2ramo1+"</td>");
+document.write("<td>"+nota3ramo1+"</td>");
+document.write("<td>"+promedioramo1+"</td>");
+document.write("<tr>");
+document.write("<tr>");
+document.write("<th scope='row'>"+ramo2+"</th>");
+document.write("<td>"+nota1ramo2+"</td>");
+document.write("<td>"+nota2ramo2+"</td>");
+document.write("<td>"+nota3ramo2+"</td>");
+document.write("<td>"+promedioramo2+"</td>");
+document.write("<tr>");
+document.write("<tr>");
+document.write("<th scope='row'>"+ramo3+"</th>");
+document.write("<td>"+nota1ramo3+"</td>");
+document.write("<td>"+nota2ramo3+"</td>");
+document.write("<td> - </td>");
+document.write("<td> - </td>");
+document.write("<tr>");
+document.write("</tbody>");
+document.write("</table>");
+document.write("<section>");
+
+//EJECUCIÓN DE FRASE *Frase con la nota necesaria para aprobar, según la nota de aprobación 
+switch(true) {
+  case nota3ramo3necesaria>7.0:
+    document.write("<div>");
+    document.write("<p>Para aprobar JavasScript, deberás hacer el curso de nuevo, debido a tus paupérrimas notas.</p>");
+    document.write("</div>");    
+    break;
+  case nota3ramo3necesaria<0:
+    document.write("<div>");
+    document.write("<p>Felicitaciones, mereces 1 año de Baltica. Para aprobar JavasScript, ni siquiera necesitas Nota 3</p>");
+    document.write("</div>");  
+    break;
+  default:
+    document.write("<div>");
+    document.write("<p>Para aprobar JavasScript con nota "+ notaaprobacion +" necesitas un "+ nota3ramo3necesaria +"  en la Nota 3.</p>");
+    document.write("</div>");
+}
+//FIN DE EJECUCIÓN DE FRASE
+
+document.write("</section>");
+document.write("</div>");
+//FIN DE DESPLIEGUE DE TABLA
+
