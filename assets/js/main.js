25083b648044a837697bc762b44671063dce5547
rosta <rodolfostazzi@gmail.com>
2022-05-17 15:31:50 -0400

    subiendo fragmento de variables .js de nombre, apellido, carrera y nota de aprobación

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+//Definimos nuestras variables con ingreso de nombre, apellido, carrera y nota de aprobación
+var nombre = prompt("Ingrese nombre: ");
+var apellido = prompt("Ingrese apellido: ");
+var carrera = prompt("Ingrese la carrera: ");
+var notaaprobacion = prompt("Ingrese nota de aprobación: ", "4.0");
